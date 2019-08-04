8db00c0d986a92720a9dd532710730f70b4c6981
Lario dos Santos Diniz <lariodiniz@gmail.com>
2019-08-04 13:26:03 -0300

    Update settings.py

--- minhamesa/settings.py
+++ minhamesa/settings.py
@@ -17,8 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '123')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
